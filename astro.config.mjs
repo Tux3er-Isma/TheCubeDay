dbf17b234cb456a96743bc51058d2d374c58585d
Tux3er <isma.tux3er@gmail.com>
2025-02-06 20:30:42 +0100

    🐛Fixing serverless problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 
+import vercel from '@astrojs/vercel/serverless';
+
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: netlify(),
+  adapter: vercel(),
 });
